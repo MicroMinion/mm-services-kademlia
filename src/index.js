71d9873bfebb255814ead576cb8bb8c46944bc82
Thomas Delaet <thomas@delaet.org>
2016-07-16 16:01:40 +0200

    switch out debug for winston logger in index.js and fix error in get method (data structure inconsistency)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 'use strict'
 
 var kademlia = require('kad')
-var debug = require('debug')('mm-services:kademlia')
 var MMTransport = require('./mm-transport.js').MMTransport
 var MMContact = require('./mm-transport.js').MMContact
 var crypto = require('./crypto.js')
+var winston = require('winston')
+var winstonWrapper = require('winston-meta-wrapper')
+var extend = require('extend.js')
 // var telemetry = require('kad-telemetry')
 
 var _ = require('lodash')
@@ -12,20 +14,32 @@ var _ = require('lodash')
 var seeds = {}
 
 var KademliaService = function (options) {
-  debug('initialize')
+  var self = this
+  this._options = options
+  if (!options.logger) {
+    this._options.logger = winston
+  }
   this.messaging = options.platform.messaging
+  this.platform = options.platform
   this.storage = options.storage
   this.myNodeInfo = {}
-  this.online = false
-  var self = this
-  this.messaging.on('self.transports.myNodeInfo', this._updateNodeInfo.bind(this))
-  options.platform.on('ready', function () {
-    self.keypair = options.platform.identity.sign
+  this._connectionsCache = []
+  this._log = winstonWrapper(this._options.logger)
+  this._log.addMeta({
+    module: 'mm-kademlia-service'
   })
+  this.online = false
+  this.messaging.on('self.transports.myNodeInfo', this._updateNodeInfo.bind(this))
+  if (options.platform.isReady()) {
+    self.keypair = options.platform.identity.sign
+  } else {
+    options.platform.on('ready', function () {
+      self.keypair = options.platform.identity.sign
+    })
+  }
 }
 
 KademliaService.prototype._updateNodeInfo = function (topic, publicKey, data) {
-  debug('_updateNodeInfo')
   this.myNodeInfo = data
   if (!this.dht) {
     this._setup()
@@ -39,7 +53,7 @@ KademliaService.prototype._updateNodeInfo = function (topic, publicKey, data) {
 }
 
 KademliaService.prototype._setup = function () {
-  debug('_setup')
+  this._log.debug('_setup')
   this.messaging.on('self.directory.get', this.get.bind(this))
   this.messaging.on('self.directory.put', this.put.bind(this))
   this.messaging.on('self.transports.nodeInfoBootstrap', this.connect.bind(this))
@@ -57,11 +71,16 @@ KademliaService.prototype._setup = function () {
   // var router = new TelemetryRouter({
   //  transport: transport
   // })
+  var kademliaLogger = winstonWrapper(this._log)
+  kademliaLogger.addMeta({
+    module: 'kad',
+    node: this.platform.identity.getSignId()
+  })
   this.dht = new kademlia.Node({
     storage: this.storage,
-    transport: transport
+    transport: transport,
+    logger: kademliaLogger
   })
-  this.dht._log.level = 3
   var service = this
   this.dht.on('connect', function () {
     service.online = true
@@ -72,76 +91,80 @@ KademliaService.prototype._setup = function () {
 }
 
 KademliaService.prototype.connect = function (topic, publicKey, data) {
-  debug('connect')
-  debug(data)
-  if (data.signId !== this.myNodeInfo.signId) {
+  if (data.signId !== this.myNodeInfo.signId && _.indexOf(this._connectionsCache, data.signId) === -1) {
+    this._log.info('connecting to node', {
+      nodeInfo: data
+    })
+    this._connectionsCache.push(data.signId)
     this.dht.connect(new MMContact(data))
   }
 }
 
 KademliaService.prototype.requestNodeInfo = function (topic, publicKey, data) {
-  debug('requestNodeInfo')
-  debug(data)
   var signId = data
   var buckets = this.dht._router._buckets
   _.forEach(buckets, function (bucket) {
     _.forEach(bucket._contacts, function (contact) {
       if (contact.nodeInfo.signId === signId) {
-        debug(contact.nodeInfo)
         this.messaging.send('transports.nodeInfo', 'local', contact.nodeInfo)
       }
     }, this)
   }, this)
 }
 
-KademliaService.prototype.get = function (topic, publicKey, data) {
-  debug('get')
+KademliaService.prototype.get = function (topic, publicKey, key) {
+  this._log.info('retrieving value', {
+    key: key
+  })
   var self = this
   if (!this.online) {
-    debug('kad server not ready to retrieve values')
+    this._log.warn('kad server not ready to retrieve values')
     return
   }
-  this.dht.get(data.key, function (err, dataObject) {
-    if (err) {
-      debug('kad server failed retrieving value for ' + data.key + '. ' + err)
+  this.dht.get(key, function (err, value) {
+    if (err || value === undefined) {
+      self._log.warn('kad server failed retrieving value', {
+        key: key,
+        error: err
+      })
       return
     }
     self.messaging.send('directory.getReply', 'local', {
-      key: data.key,
-      value: dataObject.value
+      key: key,
+      value: value
     })
   })
 }
 
 KademliaService.prototype.put = function (topic, publicKey, data) {
-  debug('put')
+  this._log.info('storing value', data)
+  var self = this
   if (!this.online) {
-    debug('kad server not ready to store KV tuples')
+    this._log.warn('kad server not ready to store values', data)
     return
   }
   if (this.online) {
     var dataObject = data.value
-    debug('kad server storing [' + data.key + ',' + JSON.stringify(dataObject) + '].')
     this.dht.put(data.key, dataObject, function (error) {
       if (error) {
-        debug('kad server failed storing [' + data.key + ',' + dataObject + ']. ' + error)
+        self._log.warn('kad server failed storing value', extend(data, {
+          error: error
+        }))
       } else {
-        debug('kad server stored [' + data.key + ',' + dataObject + ']. ')
+        self._log.info('kad server stored value', data)
       }
     })
   }
 }
 
 KademliaService.prototype._setupSeeds = function () {
-  debug('_setupSeeds')
   _.forEach(seeds, function (nodeInfo) {
     this._setupSeed(nodeInfo)
   }, this)
 }
 
 KademliaService.prototype._setupSeed = function (nodeInfo) {
-  debug('_setupSeed')
-  debug(nodeInfo)
+  this._log.debug('connecting to seed', nodeInfo)
   var self = this
   this.messaging.send('transports.nodeInfo', 'local', nodeInfo)
   setImmediate(function () {
