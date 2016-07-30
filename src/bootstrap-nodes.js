433702d302946d6db75c847734e7cc662cd71301
Thomas Delaet <thomas@delaet.org>
2016-07-30 13:43:55 +0200

    add bootstrap nodes

--- src/bootstrap-nodes.js
+++ src/bootstrap-nodes.js
@@ -1,2 +1,33 @@
-// TODO: Need to include seeds (format: dictionary with publicKey as key and connectionInfo as value)
-module.exports = {}
+module.exports = [{
+  boxId: '8YSWz6YP12esmh1+wQx83aYohcGJUY0J1VAkorvsd2w=',
+  signId: 'BTJQIayu6u0CJtH6Rr82hQODaCbH694o7GbzPFCZ9n4=',
+  connectionInfo: [{
+    transportType: 'udp',
+    transportInfo: {
+      address: 'dht-bootstrap-1.microminion.io',
+      port: 5812
+    }
+  }, {
+    transportType: 'tcp',
+    transportInfo: {
+      address: 'dht-bootstrap-1.microminion.io',
+      port: 5812
+    }
+  }]
+}, {
+  boxId: 'lqADFQ7WIC2Xl1KZ2imh9u2XXmWTwS2j7gsQkIemDAA=',
+  signId: 'qxxA1w7BO078c30pO+zZ24irlrHWFZlWjWF/vt3/HUc=',
+  connectionInfo: [{
+    transportType: 'udp',
+    transportInfo: {
+      address: 'dht-bootstrap-2.microminion.io',
+      port: 5812
+    }
+  }, {
+    transportType: 'tcp',
+    transportInfo: {
+      address: 'dht-bootstrap-2.microminion.io',
+      port: 5812
+    }
+  }]
+}]
