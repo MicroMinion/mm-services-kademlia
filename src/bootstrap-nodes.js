487c49ca6a5f69fb64ca12b4fd9415b21d85c56f
Thomas Delaet <thomas@delaet.org>
2016-07-30 15:48:07 +0200

    re-enable bootstrap nodes

--- src/bootstrap-nodes.js
+++ src/bootstrap-nodes.js
@@ -1,4 +1,3 @@
-/*
 module.exports = [{
   boxId: '8YSWz6YP12esmh1+wQx83aYohcGJUY0J1VAkorvsd2w=',
   signId: 'BTJQIayu6u0CJtH6Rr82hQODaCbH694o7GbzPFCZ9n4=',
@@ -16,24 +15,6 @@ module.exports = [{
     }
   }]
 }, {
-  boxId: 'lqADFQ7WIC2Xl1KZ2imh9u2XXmWTwS2j7gsQkIemDAA=',
-  signId: 'qxxA1w7BO078c30pO+zZ24irlrHWFZlWjWF/vt3/HUc=',
-  connectionInfo: [{
-    transportType: 'udp',
-    transportInfo: {
-      address: 'dht-bootstrap-2.microminion.io',
-      port: 5812
-    }
-  }, {
-    transportType: 'tcp',
-    transportInfo: {
-      address: 'dht-bootstrap-2.microminion.io',
-      port: 5812
-    }
-  }]
-}]
-*/
-module.exports = [{
   boxId: 'lqADFQ7WIC2Xl1KZ2imh9u2XXmWTwS2j7gsQkIemDAA=',
   signId: 'qxxA1w7BO078c30pO+zZ24irlrHWFZlWjWF/vt3/HUc=',
   connectionInfo: [{
