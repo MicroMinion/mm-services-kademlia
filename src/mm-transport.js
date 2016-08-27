8fc423896b7b0503dbbb13e94220f9c723c5e048
Thomas Delaet <thomas@delaet.org>
2016-08-27 13:16:45 +0200

    rely on mm-platform timeouts instead of kad-specific
    fixes issue #6 and #9

--- src/mm-transport.js
+++ src/mm-transport.js
@@ -3,6 +3,10 @@
 var inherits = require('inherits')
 var kademlia = require('kad')
 var crypto = require('crypto')
+var ms = require('ms')
+var assert = require('assert')
+
+var RESPONSE_TIMEOUT = ms('5s')
 
 /* KADEMLIA CONTACT */
 
@@ -39,6 +43,24 @@ var MMTransport = function (contact, options) {
 
 inherits(MMTransport, kademlia.RPC)
 
+MMTransport.prototype.open = function (callback) {
+  var self = this
+
+  if (callback) {
+    self.once('ready', callback)
+  }
+
+  self.readyState = 1
+
+  self._trigger('before:open', [], function () {
+    self._open(function () {
+      self.readyState = 2
+      self.emit('ready')
+      self._trigger('after:open')
+    })
+  })
+}
+
 MMTransport.prototype._open = function (ready) {
   this.messaging.on('self.kademlia', this._onMessage.bind(this))
   this.messaging.on('friends.kademlia', this._onMessage.bind(this))
@@ -53,11 +75,56 @@ MMTransport.prototype._onMessage = function (topic, publicKey, data) {
   this.receive(data)
 }
 
-MMTransport.prototype._send = function (data, contact) {
+MMTransport.prototype.send = function (contact, message, callback) {
+  var self = this
+
+  contact = this._createContact(contact)
+
+  assert(contact instanceof kademlia.Contact, 'Invalid contact supplied')
+  assert(message instanceof kademlia.Message, 'Invalid message supplied')
+
+  if (kademlia.Message.isRequest(message)) {
+    this._log.info('sending %s message to %j', message.method, contact)
+  } else {
+    this._log.info('replying to message to %s', message.id)
+  }
+
+  this._trigger('before:serialize', [message], function () {
+    var serialized = message.serialize()
+
+    self._trigger('after:serialize')
+    self._trigger('before:send', [serialized, contact], function () {
+      if (kademlia.Message.isRequest(message) && typeof callback === 'function') {
+        self._log.debug('queuing callback for reponse to %s', message.id)
+
+        self._pendingCalls[message.id] = {
+          timestamp: Date.now(),
+          callback: callback,
+          contact: contact,
+          message: message
+        }
+      } else {
+        self._log.debug('not waiting on callback for message %s', message.id)
+      }
+
+      self._send(message.serialize(), contact, function (err) {
+        if (err) {
+          self._log.warn('rpc call %s could not be send', message.id)
+          callback(new Error('RPC with ID `' + message.id + '` could not be send'))
+          delete self._pendingCalls[message.id]
+        }
+      })
+      self._trigger('after:send')
+    })
+  })
+}
+
+MMTransport.prototype._send = function (data, contact, callback) {
   data = JSON.parse(data.toString('utf8'))
   this.messaging.send('kademlia', contact.nodeInfo.boxId, data, {
     realtime: true,
-    expireAfter: 10000
+    expireAfter: RESPONSE_TIMEOUT,
+    callback: callback
   })
 }
 
